52c7499dfe7c9991f69eb070643d415766a0d634
Matthew Litvinov <bluefin@Matthews-MacBook-Air.local>
2021-08-05 17:53:36 -0400

    created projects card template

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,30 @@
 import React from 'react';
-import { Typography } from '@material-ui/core';
+import { Typography, makeStyles, Divider } from '@material-ui/core';
+import ProjectCard from './ProjectCard';
+import projectImageOne from '../images/portrait.jpg';
+
+const useStyles = makeStyles((theme) => ({
+  typographyTitle: {
+    fontFamily: 'Roboto',
+    color: '#DC4371',
+    fontSize: 40,
+    fontWeight: 500,
+  },
+}));
 
 const Projects = () => {
-  return <Typography>Projects Section</Typography>;
+  const classes = useStyles();
+  return (
+    <div>
+      <Typography className={classes.typographyTitle}>Projects</Typography>
+      <Divider />
+      <ProjectCard
+        image={projectImageOne}
+        title='Title'
+        description='description........................................'
+      />
+    </div>
+  );
 };
 
 export default Projects;
